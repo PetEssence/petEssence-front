b4fd14350c82b45e8195874a6304b1a5a6096e12
Isabella Miki <isabellamiki2004@gmail.com>
2025-09-08 10:38:10 -0300

    refactor(): add locale pt-BR

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import "./index.css"
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App.jsx"
+import "./index.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import locale from "antd/locale/pt_BR";
+import dayjs from "dayjs";
+import { ConfigProvider } from "antd";
+import "dayjs/locale/pt-br";
+dayjs.locale("pt-br");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ConfigProvider locale={locale}>
+      <App />
+    </ConfigProvider>
+  </React.StrictMode>
+);
