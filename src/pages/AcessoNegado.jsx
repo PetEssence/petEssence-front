09de45be4680bbfb15f914514a13e03794d9c647
Isabella Miki <isabellamiki2004@gmail.com>
2025-09-15 16:32:15 -0300

    feat(): add AcessoNegado page

---
+++ src/pages/AcessoNegado.jsx
@@ -0,0 +1,25 @@
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
+
+export default function AcessoNegado() {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-50">
+      <Result
+        status="403"
+        title="Acesso Negado"
+        subTitle="Você não tem permissão para acessar esta página."
+        extra={
+          <Button
+            type="primary"
+            className="!bg-primaryGreen !hover:bg-primaryGreenHouver"
+            onClick={() => navigate("/pet")}
+          >
+            Ir para Pets
+          </Button>
+        }
+      />
+    </div>
+  );
+}
